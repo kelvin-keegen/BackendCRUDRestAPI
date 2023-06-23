bee07e280ccc92ce49ff0d2467d6421ba4bfb103
Kelvin Keegan <kelvinkeegen17@gmail.com>
2023-06-23 14:33:17 +0100

    initial commit

---
+++ src/main/java/com/appsbykeegan/backendcrudrestapi/BackendCrudRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.appsbykeegan.backendcrudrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCrudRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendCrudRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/appsbykeegan/backendcrudrestapi/BackendCrudRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsbykeegan.backendcrudrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCrudRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
